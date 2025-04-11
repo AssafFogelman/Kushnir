b12e75eae9377b724e76ae0ae967159734edbd08
Assaf Fogelman <assaf.fogelman@gmail.com>
2025-04-11 12:08:53 +0300

    product input fields - fill other fields

--- src/pages/admin/ProductsPage.tsx
+++ src/pages/admin/ProductsPage.tsx
@@ -48,9 +48,12 @@ interface Product {
 // Mock data - replace with API call later
 const mockProducts: Product[] = [
   {
+    inceptionDate: new Date(),
     id: '1',
     name: 'ארון ספרים אורן',
-    price: 1200,
+    initialPrice: 1200,
+    currentPrice: 1200,
+    discountPercentage: 0,
     images: ['/images/products/bookshelf.jpg'],
     category: 'furniture',
     dimensions: '180x40x200 ס״מ',
@@ -63,9 +66,12 @@ const mockProducts: Product[] = [
     isHidden: false,
   },
   {
+    inceptionDate: new Date(),
     id: '2',
     name: 'שולחן אוכל אלון',
-    price: 2500,
+    initialPrice: 2500,
+    currentPrice: 2500,
+    discountPercentage: 0,
     images: ['/images/products/dining-table.jpg'],
     category: 'furniture',
     dimensions: '160x90x75 ס״מ',
@@ -84,12 +90,13 @@ const AdminProducts = () => {
   const [products, setProducts] = useState<Product[]>(mockProducts);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [editingProduct, setEditingProduct] = useState<Product | null>(null);
-  const [formData, setFormData] = useState<Partial<Product>>({
+  const [formData, setFormData] = useState<Product>({
     inceptionDate: new Date(),
+    id: '',
     name: '',
     initialPrice: 0,
-    discountPercentage: 0,
     currentPrice: 0,
+    discountPercentage: 0,
     images: [''],
     category: 'furniture',
     dimensions: '',
@@ -107,7 +114,6 @@ const AdminProducts = () => {
   const [firstWeekPriceRaisePercentage, setFirstWeekPriceRaisePercentage] = useState(
     DEFAULT_FIRST_WEEK_PRICE_PERCENTAGE
   ); //1-100
-  const [discountRate, setDiscountRate] = useState(0); //1-100
   const [taxRate, setTaxRate] = useState(18);
   const [customerPrice, setCustomerPrice] = useState(0);
 
@@ -137,9 +143,12 @@ const AdminProducts = () => {
     setIsDialogOpen(false);
     setEditingProduct(null);
     setFormData({
+      inceptionDate: new Date(),
+      id: '',
       name: '',
       initialPrice: 0,
       currentPrice: 0,
+      discountPercentage: 0,
       images: [''],
       category: 'furniture',
       dimensions: '',
@@ -153,6 +162,71 @@ const AdminProducts = () => {
     });
   };
 
+  const FIELDS = {
+    PROFIT_PERCENTAGE: 'PROFIT_PERCENTAGE',
+    CUSTOMER_PRICE: 'CUSTOMER_PRICE',
+    INITIAL_PRICE_RAISE_PERCENTAGE: 'INITIAL_PRICE_RAISE_PERCENTAGE',
+    DISCOUNT_PERCENTAGE: 'DISCOUNT_PERCENTAGE',
+    INITIAL_PRICE: 'INITIAL_PRICE',
+  } as const;
+
+  const fillOtherFields = (field: keyof typeof FIELDS, value: number) => {
+    if (taxRate < 0) return;
+    if (yourCostNoVAT < 0) return;
+    
+    const costWithTax = (taxRate/100 +1) * yourCostNoVAT;
+
+    switch (field) {
+      case FIELDS.PROFIT_PERCENTAGE:
+        setFormData({
+          ...formData,
+          currentPrice: costWithTax * (1 + value / 100),
+        });  
+        if (formData.discountPercentage > 0) {
+          setFormData({...formData, initialPrice: ?});
+          setFirstWeekPriceRaisePercentage(?);          
+        }
+        if (firstWeekPriceRaisePercentage > 0) {
+          setFormData({...formData, initialPrice: ?});
+          setFormData({...formData, discountPercentage: ?});
+        }
+        if (formData.initialPrice > 0) {
+          setFirstWeekPriceRaisePercentage(?);          
+          setFormData({...formData, discountPercentage: ?});
+        }
+        break;
+      case FIELDS.INITIAL_PRICE:
+        setProfitPercentage(100 * (value / costWithTax - 1));
+        if (formData.discountPercentage > 0) {
+          setFormData({...formData, initialPrice: ?});
+          setFirstWeekPriceRaisePercentage(?);          
+        }
+        if (firstWeekPriceRaisePercentage > 0) {
+          setFormData({...formData, initialPrice: ?});
+          setFormData({...formData, discountPercentage: ?});
+        }
+        if (formData.initialPrice > 0) {
+          setFirstWeekPriceRaisePercentage(?);          
+          setFormData({...formData, discountPercentage: ?});
+        }
+        break;
+      case FIELDS.INITIAL_PRICE_RAISE_PERCENTAGE:
+        setFormData({...formData, discountPercentage: ?});
+        if(profitPercentage > 0) {
+          setFormData({...formData, initialPrice: ?});
+        } //we don't need to check if there is a current price because when you enter it, the profit percentage will also be determined.
+        //and we can't know the initial price if we don't have either the current price or the profit percentage.
+        break;
+      case FIELDS.DISCOUNT_PERCENTAGE:
+      setFirstWeekPriceRaisePercentage(?);
+      if(profitPercentage > 0) {
+        setFormData({...formData, initialPrice: ?});
+      }//we don't need to check if there is a current price because when you enter it, the profit percentage will also be determined.
+        //and we can't know the initial price if we don't have either the current price or the profit percentage.
+      break;
+        
+    }
+  };
   const setInitialPrice = () => {
     const initialPrice =
       yourCostNoVAT *
@@ -188,13 +262,16 @@ const AdminProducts = () => {
               {t('adminProducts.addProduct')}
             </Button>
           </DialogTrigger>
-          <DialogContent>
+          <DialogContent dir='auto' className='text-start'>
             <DialogHeader>
-              <DialogTitle>
+              <DialogTitle className='text-start'>
                 {editingProduct ? t('adminProducts.editProduct') : t('adminProducts.addProduct')}
               </DialogTitle>
             </DialogHeader>
-            <form onSubmit={handleSubmit} className='space-y-4'>
+            <form
+              onSubmit={handleSubmit}
+              className='grid grid-cols-2 gap-4 [&>*]:rtl:text-right [&>*]:ltr:text-left'
+            >
               <div>
                 <label className='block text-sm font-medium mb-1'>{t('adminProducts.name')}</label>
                 <Input
@@ -203,20 +280,6 @@ const AdminProducts = () => {
                   required
                 />
               </div>
-              {/* <div>
-                <label className='block text-sm font-medium mb-1'>{t('adminProducts.price')}</label>
-                <Input
-                  type='number'
-                  value={formData.price}
-                  onChange={e =>
-                    setFormData({
-                      ...formData,
-                      price: parseFloat(e.target.value),
-                    })
-                  }
-                  required
-                />
-              </div> */}
               <div>
                 <label className='block text-sm font-medium mb-1'>
                   {t('adminProducts.yourCostNoVAT')}
@@ -391,7 +454,7 @@ const AdminProducts = () => {
                   onChange={e => setFormData({ ...formData, material: e.target.value })}
                 />
               </div>
-              <div>
+              <div className='col-span-2'>
                 <label className='block text-sm font-medium mb-1'>
                   {t('adminProducts.description')}
                 </label>
@@ -445,27 +508,29 @@ const AdminProducts = () => {
                   }
                 />
               </div>
-              <div>
-                <label className='block text-sm font-medium mb-1'>
-                  {t('adminProducts.inStock')}
-                </label>
-                <Input
-                  type='checkbox'
-                  checked={formData.inStock}
-                  onChange={e => setFormData({ ...formData, inStock: e.target.checked })}
-                />
+              <div className='flex items-center gap-8'>
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    {t('adminProducts.inStock')}
+                  </label>
+                  <Input
+                    type='checkbox'
+                    checked={formData.inStock}
+                    onChange={e => setFormData({ ...formData, inStock: e.target.checked })}
+                  />
+                </div>
+                <div>
+                  <label className='block text-sm font-medium mb-1'>
+                    {t('adminProducts.isHidden')}
+                  </label>
+                  <Input
+                    type='checkbox'
+                    checked={formData.isHidden}
+                    onChange={e => setFormData({ ...formData, isHidden: e.target.checked })}
+                  />
+                </div>
               </div>
-              <div>
-                <label className='block text-sm font-medium mb-1'>
-                  {t('adminProducts.isHidden')}
-                </label>
-                <Input
-                  type='checkbox'
-                  checked={formData.isHidden}
-                  onChange={e => setFormData({ ...formData, isHidden: e.target.checked })}
-                />
-              </div>
-              <Button type='submit' className='w-full'>
+              <Button type='submit' className='col-span-2 mt-4'>
                 {editingProduct ? t('adminProducts.update') : t('adminProducts.add')}
               </Button>
             </form>
@@ -477,7 +542,9 @@ const AdminProducts = () => {
         <TableHeader>
           <TableRow>
             <TableHead className='text-right'>{t('adminProducts.name')}</TableHead>
-            <TableHead className='text-right'>{t('adminProducts.price')}</TableHead>
+            <TableHead className='text-right'>{t('adminProducts.currentPrice')}</TableHead>
+            <TableHead className='text-right'>{t('adminProducts.initialPrice')}</TableHead>
+            <TableHead className='text-right'>{t('adminProducts.discountPercentage')}</TableHead>
             <TableHead className='text-right'>{t('adminProducts.category')}</TableHead>
             <TableHead className='text-right'>{t('adminProducts.actions')}</TableHead>
           </TableRow>
@@ -487,7 +554,7 @@ const AdminProducts = () => {
             <TableRow key={product.id}>
               <TableCell>{product.name}</TableCell>
               <TableCell>
-                {product.price} {t('shekel')}
+                {product.initialPrice} {t('shekel')}
               </TableCell>
               <TableCell>{product.category}</TableCell>
               <TableCell>
